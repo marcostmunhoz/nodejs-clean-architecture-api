c95f613f71d99e9f9908e577d50d75b08bbe951c
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2021-09-22 20:31:34 -0300

    feat: ensure SignUpController returns 500 if theres an unhandled error

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,7 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest, success } from '../helpers/http-helper'
+import { ServerError } from '../errors/server-error'
+import { badRequest, success, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { HttpResponse, HttpRequest } from '../protocols/http'
@@ -9,18 +10,23 @@ export class SignUpController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
 
   handle (request: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
-    for (const field of requiredFields) {
-      if (!request.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!request.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    if (!this.emailValidator.isValid(request.body.email)) {
-      return badRequest(new InvalidParamError('email'))
-    }
+      const isValid = this.emailValidator.isValid(request.body.email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
 
-    return success({})
+      return success({})
+    } catch (error) {
+      return serverError(new ServerError())
+    }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,11 +1,16 @@
 import { HttpResponse } from '../protocols/http'
 
+export const success = (body: any): HttpResponse => ({
+  statusCode: 200,
+  body
+})
+
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
 
-export const success = (body: any): HttpResponse => ({
-  statusCode: 200,
-  body
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: error
 })
