b16ce21e9a4584c111b1f71c073632226ce4568b
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2021-10-04 20:37:58 -0300

    feat: integrate AccountMongoRepository with MongoDB

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,17 @@
+import { AddAccountRepository } from '@/data/protocols'
+import { AccountModel } from '@/domain/models/account'
+import { AddAccountModel } from '@/domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async create (addAccountModel: AddAccountModel): Promise<AccountModel> {
+    const collection = MongoHelper.getCollection('accounts')
+
+    const result = await collection.insertOne(addAccountModel)
+    const document = await collection.findOne({ _id: result.insertedId })
+    // @ts-expect-error
+    const { _id, ...documentWithoutId } = document
+
+    return Object.assign({}, documentWithoutId, { id: _id }) as AccountModel
+  }
+}
