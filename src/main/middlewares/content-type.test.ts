ea512fed525485b4219b119ba895ed6a000c200f
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2021-10-06 20:31:33 -0300

    test: ensure application returns another content type when explicitly set

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,12 +3,22 @@ import app from '../config/app'
 
 describe('Content Type Middleware', () => {
   test('Should return default content type as JSON', async () => {
-    app.get('/test_content_type', (req, res) => {
+    app.get('/test_content_type_json', (req, res) => {
       res.send()
     })
 
     await request(app)
-      .get('/test_content_type')
+      .get('/test_content_type_json')
       .expect('content-type', /json/)
+  })
+
+  test('Should return XML content type when explicitly set', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml').send()
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
