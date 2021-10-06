5ef1a474fc0ef31e5d2bdb0fd3c6f63f4e0ec1e2
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2021-10-06 20:27:27 -0300

    test: fix cors test route method

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,12 +3,12 @@ import app from '../config/app'
 
 describe('CORS Middleware', () => {
   test('Should enable CORS', async () => {
-    app.post('/test_cors', (req, res) => {
+    app.get('/test_cors', (req, res) => {
       res.send()
     })
 
     await request(app)
-      .post('/test_cors')
+      .get('/test_cors')
       .expect('access-control-allow-origin', '*')
       .expect('access-control-allow-methods', '*')
       .expect('access-control-allow-headers', '*')
