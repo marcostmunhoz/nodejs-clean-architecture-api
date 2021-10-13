bd11000313a5338813a53940823345e24ce8b4b3
Marcos Tesolin Munhoz <marmunhoz414@gmail.com>
2021-10-13 20:24:02 -0300

    refactor: reset client to null after disconnecting MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -25,6 +25,8 @@ export default class MongoHelper {
 
   async disconnect (): Promise<void> {
     await this.client?.close()
+
+    this.client = null
   }
 
   async getCollection (name: string): Promise<Collection> {
